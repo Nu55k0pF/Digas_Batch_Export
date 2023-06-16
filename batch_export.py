9fc7c89478151d22f72ee3b4a5f275268ca16524
Nu55k0pF <madironbrain@googlemail.com>
2023-06-16 14:52:39 +0200

    blah

---
+++ batch_export.py
@@ -0,0 +1,16 @@
+import mysql
+
+def read_list() -> list:
+    """"""
+    return 0
+
+def search_database() -> str:
+    """"""
+    return "path_to_file_on_local_server"
+
+## Save all paths in list of lists
+
+def creat_m3u():
+    """schreibt eine m3u playliste"""
+    
+
