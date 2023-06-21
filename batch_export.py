abf51ab2504a7ab3cc0e49297f65927d682bd822
Nu55k0pF <madironbrain@googlemail.com>
2023-06-21 08:47:29 +0200

    started work on db query func

--- batch_export.py
+++ batch_export.py
@@ -1,4 +1,5 @@
-import mysql
+import mysql.connector
+from getpass import getpass
 
 def read_list() -> list:
     """"""
@@ -9,9 +10,40 @@ def read_list() -> list:
         
     return songs
 
-def search_database() -> str:
+def search_database(title) -> str:
     """"""
+    # Connect to the database
+    # host = input("Host: ")
+    # user = input("User: ")
+    password = getpass("Enter your Password: ")
+    # host = input("Host: ")
+    
+    mydb = mysql.connector.connect(
+        host="prodserv4",
+        user="digas",
+        password=password,
+        database="digas"
+    )
+
+    # Create a cursor object
+    mycursor = mydb.cursor()
+
+    # Execute the query
+    sql = "SELECT * FROM musik_934_t and musik_934 WHERE (TITLE LIKE '%s');"
+    val = ("%" + title + "%", )
+    mycursor.execute(sql, val)
+
+    # Fetch the results
+    results = mycursor.fetchall()
+
+    # return results
+
+    # Print the results
+    for result in results:
+        print(result)
     return "path_to_file_on_local_server"
+
+
 
 ## Save all paths in list of lists
 
@@ -21,7 +53,8 @@ def creat_m3u():
 
 def main():
     test = read_list()
-    print(test)
+    for song in test:
+        search_database(song)
 
 if __name__ == "__main__":
     main()
